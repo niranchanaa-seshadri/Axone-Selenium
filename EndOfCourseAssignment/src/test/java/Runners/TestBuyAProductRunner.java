c7185a482a98a1135863708b0da78f72747610e3
niranchanaa-seshadri <125280025+niranchanaa-seshadri@users.noreply.github.com>
2024-11-06 13:46:28 +0000

    Update and rename buyAProductRunner.java to TestBuyAProductRunner.java

--- EndOfCourseAssignment/src/test/java/Runners/TestBuyAProductRunner.java
+++ EndOfCourseAssignment/src/test/java/Runners/TestBuyAProductRunner.java
@@ -6,5 +6,5 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions (features = "src/test/resources/Features/buyAProduct.feature",
 glue = "StepDefs",
 plugin = {"pretty", "html:target/reports/report.html"})
-public class buyAProductRunner extends AbstractTestNGCucumberTests {
+public class TestBuyAProductRunner extends AbstractTestNGCucumberTests {
 }
